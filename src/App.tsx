97d07cc2cf66074e67f930395a4c67b90c47a94a
Guilherme Ribeiro <guilherme_ribeiro52@icloud.com>
2024-11-05 12:24:50 -0300

    Configurações iniciais

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<div className="bg-gradientBlue text-displayLargeBold text-purple100">
+  HBO Max Clone
+</div>
+
+<p className="text-displayMediumRegular text-gray300 font-sintony">
+  Aproveite nosso conteúdo exclusivo
+</p>
+
+<button className="bg-blueAlt text-white py-2 px-4 rounded font-poppins font-semibold">
+  Comece a Assistir
+</button>
+
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple100: '#5b1ce6',
+        purple500: '#9E86FF',
+        gray500: '#F0F0F0',
+        gray200: '#999999',
+        gray300: '#3C3C3C',
+      },
+      backgroundImage: {
+        gradientBlue: 'linear-gradient(0deg, #1400FF, #72AAFF)',
+        blueAlt: 'linear-gradient(0deg, #4710C1, #7857FF, #819BFD)',
+        gradientPink: 'linear-gradient(1deg, #FC00C4, #FF8CE6)',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        sintony: ['Sintony', 'sans-serif'],
+      },
+      fontSize: {
+        displayLargeRegular: ['48px', { fontWeight: '400' }],
+        displayMediumRegular: ['32px', { fontWeight: '400' }],
+        displaySmallRegular: ['22px', { fontWeight: '400' }],
+        displayLargeBold: ['48px', { fontWeight: '700' }],
+        displayMediumBold: ['32px', { fontWeight: '700' }],
+        displaySmallBold: ['22px', { fontWeight: '700' }],
+        textLargeRegular: ['16px', { fontWeight: '400' }],
+        textMediumRegular: ['14px', { fontWeight: '400' }],
+        textMediumSmall: ['10px', { fontWeight: '400' }],
+        textLargeBold: ['16px', { fontWeight: '700' }],
+        textMediumBold: ['14px', { fontWeight: '700' }],
+        textMediumSmallBold: ['10px', { fontWeight: '700' }],
+      },
+    },
+  },
+  plugins: [],
+}
