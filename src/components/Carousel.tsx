aa36609b13c88a7490a49082b6e8c9c7504d399f
Guilherme Ribeiro <guilherme_ribeiro52@icloud.com>
2024-11-26 12:42:42 -0300

    fix - Melhorando a responsividade dos carrosséis

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -25,13 +25,13 @@ const Carousel: React.FC<CarouselSectionProps> = ({ title, items, type, isLarge 
     speed: 500,
     slidesToShow: isLarge ? 4 : 5,
     slidesToScroll: 1,
-    autoplay: true, 
+    autoplay: true,
     autoplaySpeed: 5000,
     responsive: [
-      { breakpoint: 1440, settings: { slidesToShow: isLarge ? 3 : 5, slidesToScroll: isLarge? 1 : 4 } },
-      { breakpoint: 1024, settings: { slidesToShow: isLarge ? 1.5 : 3, slidesToScroll: isLarge? 1 : 3 } },
-      { breakpoint: 768, settings: { slidesToShow: isLarge ? 1.5 : 2.5, slidesToScroll: isLarge? 1 : 2} },
-      { breakpoint: 640, settings: { slidesToShow: isLarge ? 1 : 2, slidesToScroll: isLarge? 1 : 2 } },
+      { breakpoint: 1440, settings: { slidesToShow: isLarge ? 3 : 5, slidesToScroll: 1 } },
+      { breakpoint: 1024, settings: { slidesToShow: isLarge ? 2 : 3, slidesToScroll: 1 } },
+      { breakpoint: 768, settings: { slidesToShow: isLarge ? 2 : 3, slidesToScroll: 1 } },
+      { breakpoint: 640, settings: { slidesToShow: isLarge ? 1 : 2, slidesToScroll: 1, arrows: false } },
       { breakpoint: 400, settings: { slidesToShow: 1, slidesToScroll: 1, arrows: false } },
     ],
   };
@@ -41,17 +41,19 @@ const Carousel: React.FC<CarouselSectionProps> = ({ title, items, type, isLarge 
       <h2 className="text-xl font-semibold mb-4 text-white">{title}</h2>
       <Slider {...settings}>
         {items.map((item) => (
-          <div key={item.id}>
+          <div key={item.id} className="px-2"> 
             {renderItem ? (
               renderItem(item) 
             ) : (
               <Link to={`/${type}/${item.id}`}>
-                <img
-                  src={`https://image.tmdb.org/t/p/${isLarge ? 'w780' : 'w300'}${isLarge ? item.backdrop_path : item.poster_path}`}
-                  alt={item.title || item.name}
-                  className={`rounded-lg cursor-pointer ${isLarge ? 'max-w-[400px] max-h-[281px]' : 'w-[240px] h-[361px]'}`}
-                />
-                <p className={`text-center ${isLarge ? 'max-w-[500px]' : 'max-w-[240px]'}`}>
+                <div className="relative w-full h-0 pb-[150%] overflow-hidden">
+                  <img
+                    src={`https://image.tmdb.org/t/p/${isLarge ? 'w780' : 'w300'}${isLarge ? item.backdrop_path : item.poster_path}`}
+                    alt={item.title || item.name}
+                    className="absolute top-0 left-0 w-full h-full object-cover rounded-lg cursor-pointer"
+                  />
+                </div>
+                <p className="text-center mt-2">
                   {item.title || item.name}
                 </p>
               </Link>
